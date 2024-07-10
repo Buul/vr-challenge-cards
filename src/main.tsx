28dfc48b786e8373d9ff8e4a7bb4ed19dc1429d5
Paulo Silva <paulo.firmino2@gmail.com>
2024-07-10 01:32:20 -0300

    fix(Redux): delete provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import ReactDOM from 'react-dom/client';
 
-import { Cards } from './components/Cards/Cards';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <div style={{ display: 'flex', width: '100vw', justifyContent: 'center' }}>
-    <Cards />
-  </div>
+  <div />
 );
